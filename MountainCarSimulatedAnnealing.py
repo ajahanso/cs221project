c0e0869a678cdf2b7bb54c61f9e330bb4545c1bb
Joe-Vincent <33035786+Joe-Vincent@users.noreply.github.com>
2018-12-13 19:03:07 -0800

    Update MountainCarSimulatedAnnealing.py

--- MountainCarSimulatedAnnealing.py
+++ MountainCarSimulatedAnnealing.py
@@ -1,5 +1,8 @@
 '''
 This code tests a policy in the CartPole_V0 environment
+
+format for weight vector update and implementation from:
+https://github.com/kvfrans/openai-cartpole
 '''
 
 import math
